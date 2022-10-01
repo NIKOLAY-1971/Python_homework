9ff50c2e990c173984e9a770655f29e9bb3db05e
Nikolay <rnm71@yandex.ru>
2022-10-02 00:51:39 +0300

    35

--- Home3/task3.py
+++ Home3/task3.py
@@ -1,15 +1,17 @@
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 # Пример:
-# - 45 -> 101101
-# - 3 -> 11
-# - 2 -> 10
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-x = int(input('Введите число: '))
-kount = 0
-number = 0
-while x != 0:
-    x1 = int(x % 2)
-    x = int(x / 2)
-    number += x1 * 10 ** kount
-    kount += 1
-print(number)
+n = int(input('Введите число: '))
+list = []
+list1 = []
+for i in range(0, n+1):
+        if i == 0:
+            list.append(0)
+        elif i == 1 or i ==2:
+            list.append(1)
+        else:
+            list.append(list[i-1] + list[i-2])
+for i in range(1,n+1):
+    list1.append(-1*list[n+1-i])
+print(list1+list) 
